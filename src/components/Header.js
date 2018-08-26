6a62363f9ba768c4860e69c2aa1ef9779c6e9274
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-08-25 20:46:07 -0500

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => (
+  <header>
+    <div className="container text-center">
+      <h1>Expirtation Date</h1>
+      <h2>Jot down when an item expires</h2>
+    </div>
+  </header>
+)
+
+export default Header
